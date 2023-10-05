dd034b37719f4379813e567719287b5cb8ff0024
brycebien <brycebien@gmail.com>
2023-10-05 10:19:13 -0400

    merged login and sign up to one page split horizontally

--- lib/auth/login.php
+++ lib/auth/login.php
@@ -5,12 +5,24 @@
     </head>
 
     <body>
+        <h3>Log In</h3>
         <form method="POST">
             <label for="Username">Username:</label><br>
             <input type="text" name="Username"><br>
             <label for="Password">Password:</label><br>
             <input type="text" name="Password"><br>
             <input type="submit" value="Login">
+        </form>
+        <hr>
+        <h3>Sign Up</h3>
+        <form method="POST">
+            <label for="Username">Enter a username:</label><br>
+            <input type="text" name="Username"><br>
+            <label for="Password">Enter a password:</label><br>
+            <input type="text" name="Password"><br>
+            <label for="ConfirmPassword">Confirm your password:</label><br>
+            <input type="text" name="ConfirmPassword"><br>
+            <input type="submit" value="Sign Up">
         </form>
     </body>
 </html>
