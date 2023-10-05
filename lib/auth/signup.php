755b3242ca5e17d1bc8084564adde93794d1a7dd
brycebien <brycebien@gmail.com>
2023-10-05 10:06:21 -0400

    created a basic sign up page

---
+++ lib/auth/signup.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sign up</title>
+    </head>
+
+    <body>
+        <form method="POST">
+            <label for="Username">Enter a username:</label><br>
+            <input type="text" name="Username"><br>
+            <label for="Password">Enter a password:</label><br>
+            <input type="text" name="Password"><br>
+            <label for="ConfirmPassword">Confirm your password:</label><br>
+            <input type="text" name="ConfirmPassword"><br>
+        </form>
+    </body>
+</html>
