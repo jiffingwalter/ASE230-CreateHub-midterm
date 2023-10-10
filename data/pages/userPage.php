8997afb58e116ceebf6f02592be4a6555215585e
brycebien <brycebien@gmail.com>
2023-10-10 10:13:22 -0400

    made it where the nav bar makes sense
    added nav.php to themes

--- data/pages/userPage.php
+++ data/pages/userPage.php
@@ -4,18 +4,10 @@ require_once('../themes/head.php');
 ?>
 
 <body id="page-top" style="background-color: black;">
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="./index.php">Create Hub</a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./portfolio.php?index=<?=$index?>">Portfolio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php
+    require_once('../themes/nav.php');
+    ?>
+    <div style="margin-top: 70px;">
+        <h1><a class="nav-link" href="./portfolio.php?index=<?=$index?>">Portfolio</a></h1>
+    </div>
 </body>
