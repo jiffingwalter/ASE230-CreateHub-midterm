11115e87f396910da720be57182da417fcd9ba15
brycebien <brycebien@gmail.com>
2023-12-12 22:03:45 -0500

    edit portfolio name

--- data/pages/postSettings/editPortfolio.php
+++ data/pages/postSettings/editPortfolio.php
@@ -5,13 +5,24 @@ require_once($GLOBALS['databaseDirectory']);
 require_once($GLOBALS['postHandlingDirectory']);
 $index = $_GET['index'];
 $portfolios=get_user_portfolio($userID);
+
+if(count($_POST)>0){
+    if($_POST['title'] != 0){
+        $changeTitle = db->preparedQuery('UPDATE portfolios SET name = ? WHERE fid = ?',[$_POST['title'],$portfolios[$index]['fid']]);
+        header('Location: ../portfolio.php');
+    }
+}
 ?>
 <a href="../portfolioSelect.php?index=<?=$index?>"><< BACK </a>
 
 <?php
 //add a way to add content to portfilio
 ?>
-
+<form method="POST" action="editPortfolio.php?index=<?=$index?>">
+    <h3>Title: <?=$portfolios[$index]['name']?></h3>
+    <input type="text" name="title">
+    <input type="submit" value="Change Title">
+</form>
 <h3> Click on a photo to delete </h3>
 <?php
 for($i=0;$i<count($portfolios[$index]['images']);$i++){
